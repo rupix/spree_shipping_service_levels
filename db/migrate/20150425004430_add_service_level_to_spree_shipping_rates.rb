3a53512eb431e5d21e752eff8c699c38799c4522
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-04-28 19:49:46 -0600

    Fix shipping rates service level migration

--- db/migrate/20150425004430_add_service_level_to_spree_shipping_rates.rb
+++ db/migrate/20150425004430_add_service_level_to_spree_shipping_rates.rb
@@ -1,5 +1,5 @@
 class AddServiceLevelToSpreeShippingRates < ActiveRecord::Migration
   def change
-    add_column :spree_shipping_rates, :service_level_id, default: nil
+    add_column :spree_shipping_rates, :service_level_id, :integer, default: nil
   end
 end
