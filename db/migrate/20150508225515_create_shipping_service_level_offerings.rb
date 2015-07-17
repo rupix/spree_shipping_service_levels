e719f51d5cb8e8e3ba887c850cca5d4c9c6100d6
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-07-17 16:34:04 -0600

    Remove foreign keys in migrations

--- db/migrate/20150508225515_create_shipping_service_level_offerings.rb
+++ db/migrate/20150508225515_create_shipping_service_level_offerings.rb
@@ -1,8 +1,8 @@
 class CreateShippingServiceLevelOfferings < ActiveRecord::Migration
   def change
     create_table :spree_shipping_service_level_offerings do |t|
-      t.belongs_to :shipping_service_level, foreign_key: true
-      t.belongs_to :stock_location, foreign_key: true
+      t.belongs_to :shipping_service_level
+      t.belongs_to :stock_location
     end
 
     add_index :spree_shipping_service_level_offerings, :shipping_service_level_id, name: 'service_level_offerings_service_level_index'
--- db/migrate/20150508225629_create_shipping_method_offerings.rb
+++ db/migrate/20150508225629_create_shipping_method_offerings.rb
@@ -1,8 +1,8 @@
 class CreateShippingMethodOfferings < ActiveRecord::Migration
   def change
     create_table :spree_shipping_method_offerings do |t|
-      t.belongs_to :shipping_method, foreign_key: true
-      t.belongs_to :stock_location, foreign_key: true
+      t.belongs_to :shipping_method
+      t.belongs_to :stock_location
     end
 
     add_index :spree_shipping_method_offerings, :shipping_method_id, name: 'shipping_method_offerings_method_index'
