4786093018de57403b5c93efbdca3e05540394c3
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-07-28 14:24:16 -0600

    Fix some tests

--- spec/models/spree/shipping/time_adjuster_spec.rb
+++ spec/models/spree/shipping/time_adjuster_spec.rb
@@ -8,18 +8,18 @@ module Spree::Shipping
     context '#adjusted_date' do
       it 'calculates correctly with a start time that is a blacked out date' do
         start_time = DateTime.new(2015,12,25,12,30,00)
-        expect(adjuster.adjusted_date(start_time, days)).to eq(DateTime.new(2015,12,28,18,00,00))
+        expect(adjuster.adjusted_date(start_time, days)).to eq(DateTime.new(2015,12,29,12,30,00))
       end
-      it 'calculates correctly with a start time that is a blacked out date' do
-        start_time = DateTime.new(2015,12,25,12,30,00)
-        expect(adjuster.adjusted_date(start_time, days)).to eq(DateTime.new(2015,12,28,18,00,00))
+      it 'calculates correctly with a start time that is a blacked out weekday' do
+        start_time = DateTime.new(2015,8,2,12,30,00)
+        expect(adjuster.adjusted_date(start_time, days)).to eq(DateTime.new(2015,8,4,12,30,00))
       end
 
       context 'with slow processing' do
         let(:days){4}
         it 'calculates correctly with processing straddling consecutive blacked out days' do
-          start_time = DateTime.new(2015,12,22,11,30)
-          expect(adjuster.adjusted_date(start_time, days)).to eq(DateTime.new(2015,12,30,18,00,00))
+          start_time = DateTime.new(2015,12,22,12,30)
+          expect(adjuster.adjusted_date(start_time, days)).to eq(DateTime.new(2015,12,30,12,30,00))
         end
       end
       
@@ -27,7 +27,7 @@ module Spree::Shipping
         let(:days){10}
         it 'adds the time for both weekends' do
           start_time = DateTime.new(2015,6,8,11,30)
-          expect(adjuster.adjusted_date(start_time, days)).to eq(DateTime.new(2015,6,22,18))
+          expect(adjuster.adjusted_date(start_time, days)).to eq(DateTime.new(2015,6,22,11,30))
         end
       end
     end
