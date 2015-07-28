5daf89234801d5b5da2d0f00c7253c3b593c54f9
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-07-28 14:24:48 -0600

    Fix outdated time adjuster factory

--- spec/factories/spree/shipping/time_adjuster_factory.rb
+++ spec/factories/spree/shipping/time_adjuster_factory.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :time_adjuster, class: Spree::Shipping::TimeAdjuster do
-    initialize_with{new(['0','6'],['12/24','12/25'])}
+    initialize_with{new(Spree::Shipping::Blackout.from_strings("0,6","12/24,12/25"))}
   end
 end
