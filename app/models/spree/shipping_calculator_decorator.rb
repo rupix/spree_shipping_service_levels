63c787b6290dcacf7376ec2951498c2441cb24d6
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-07-31 17:55:28 -0600

    Return the preferred rate daily expiration hour by default

--- app/models/spree/shipping_calculator_decorator.rb
+++ app/models/spree/shipping_calculator_decorator.rb
@@ -1,3 +1,7 @@
 Spree::ShippingCalculator.class_eval do
   preference :rate_daily_expiration_hour, :decimal, default: nil
+
+  def rate_daily_expiration_hour
+    preferred_rate_daily_expiration_hour
+  end
 end
