a72d3277e48230b7f96dc0ac77e5d7df58ad2bfd
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-08-11 12:56:26 -0600

    Also check for expired shipping rates each time a checkout page is loaded

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,7 +1,20 @@
 Spree::CheckoutController.class_eval do
-  around_action :catch_shipment_validation_errors, only: [:update]
+  before_action :check_for_expired_shipping_rates
+  around_action :catch_shipment_validation_errors_filter, only: [:update]
 
   private
+
+  def check_for_expired_shipping_rates
+    catch_shipment_validation_errors do
+      @order.ensure_valid_shipments
+    end
+  end
+
+  def catch_shipment_validation_errors_filter
+    catch_shipment_validation_errors do
+      yield
+    end
+  end
 
   def catch_shipment_validation_errors
     begin
