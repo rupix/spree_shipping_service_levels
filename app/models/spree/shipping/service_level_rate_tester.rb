bbfb321096d04df90fc6db988ba0e65c6407698e
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-08-19 16:43:28 -0600

    Take service level minimum delivery dates into account when testing rates

--- app/models/spree/shipping/service_level_rate_tester.rb
+++ app/models/spree/shipping/service_level_rate_tester.rb
@@ -15,12 +15,18 @@ module Spree::Shipping
     attr_reader :service_level, :rate, :ship_time
 
     def meets_latest_delivery_date_with?(rate)
-      (rate && rate.delivery_window && rate.delivery_window.end.nil?) || rate.delivery_window.end <= latest_deliver_date
+      (rate && rate.delivery_window && rate.delivery_window.end.nil?) || (rate.delivery_window.end <= latest_deliver_date.end_of_day && rate.delivery_window.start >= earliest_deliver_date.beginning_of_day)
     end
 
     def latest_deliver_date
       @latest_deliver_date ||= begin
-        delivery_time_adjuster.adjusted_date(ship_time, service_level.days_to_deliver_max)
+        delivery_time_adjuster.adjusted_date(ship_time, service_level.days_to_deliver_max).end_of_day
+      end
+    end
+    
+    def earliest_deliver_date
+      @earliest_deliver_date ||= begin
+        delivery_time_adjuster.adjusted_date(ship_time, service_level.days_to_deliver_min).beginning_of_day
       end
     end
 
