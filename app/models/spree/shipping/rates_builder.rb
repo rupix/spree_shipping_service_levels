b642ebbcd072f6b1037f27c3ab50b2b8d2c6054e
Kyle Van Wagenen <kvanwagenen@gmail.com>
2015-08-24 21:55:48 +0000

    Add caching when building rates to speed up calculation.

--- app/models/spree/shipping/rates_builder.rb
+++ app/models/spree/shipping/rates_builder.rb
@@ -46,19 +46,22 @@ module Spree
       end
 
       def rates_for_service_level(service_level)
-        viable_calculated_rates(service_level).select do |rate|
+        viable_calculated_rates(service_level.processing_days).select do |rate|
           rate_tester(service_level).passes_with?(rate)
         end
       end
 
-      def viable_calculated_rates(service_level)
-        viable_shipping_methods.map do |shipping_method|
-          rate_for(shipping_method, service_level)
+      def viable_calculated_rates(processing_days)
+        @rates_for_processing_days ||= {}
+        @rates_for_processing_days[processing_days] ||= begin
+          viable_shipping_methods.map do |shipping_method|
+            rate_for(shipping_method, processing_days)
+          end
         end
       end
 
       def viable_shipping_methods
-        package.stock_location.shipping_methods.select do |method|
+        @viable_shipping_methods ||= package.stock_location.shipping_methods.select do |method|
           viable_shipping_method?(method)
         end
       end
@@ -67,28 +70,28 @@ module Spree
         method.can_ship_to?(package.order.ship_address) && method.can_ship?(package)
       end
 
-      def rate_for(shipping_method, service_level)
-        RateBuilder.new(package, shipping_method, ship_time(service_level)).rate
+      def rate_for(shipping_method, processing_days)
+        RateBuilder.new(package, shipping_method, ship_time(processing_days)).rate
       end
 
-      def ship_time(service_level)
+      def ship_time(processing_days)
         @ship_times ||= {}
-        @ship_times[service_level] ||= calculate_ship_time(service_level)
+        @ship_times[processing_days] ||= calculate_ship_time(processing_days)
       end
 
-      def calculate_ship_time(service_level)
+      def calculate_ship_time(processing_days)
         ShipTimeCalculator.new(
           Time.now,
           stock_location.same_day_cutoff_hour,
           stock_location.latest_daily_ship_hour,
-          service_level.processing_days,
+          processing_days,
           stock_location.processing_blackout
         ).ship_time
       end
 
       def rate_tester(service_level)
         @rate_testers ||= {}
-        @rate_testers[service_level] ||= ServiceLevelRateTester.new(service_level, ship_time(service_level))
+        @rate_testers[service_level] ||= ServiceLevelRateTester.new(service_level, ship_time(service_level.processing_days))
       end   
 
     end
